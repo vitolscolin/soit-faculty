f1cefec1a3d3f283fce77f5943e8a27e2bec1e0d
Colin Vitols <vitolscn@mail.uc.edu>
2021-09-30 14:47:02 -0400

    intial commit of project

---
+++ soitfaculty/src/main/java/com/soit/soitfaculty/FacultyController.java
@@ -0,0 +1,11 @@
+package com.soit.soitfaculty;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/Faculties")
+public class FacultyController {
+
+	
+}
---
+++ soitfaculty/src/main/java/com/soit/soitfaculty/SoitfacultyApplication.java
@@ -0,0 +1,13 @@
+package com.soit.soitfaculty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoitfacultyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoitfacultyApplication.class, args);
+	}
+
+}
